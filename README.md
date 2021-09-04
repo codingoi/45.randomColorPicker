240955a0541004c6992d8e81ae4e842911bbe148
osita-igwe <valorcourt.international@gmail.com>
2021-09-03 20:59:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-v9j8wy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-v9j8wy)
---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../style.css';
+
+import Random from '../Random/Random.js';
+
+export default function App() {
+  return (
+    <div>
+      <Random />
+    </div>
+  );
+}
